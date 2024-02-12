5eebbaebe984afe86b0bce58c44ba3b23e80b116
HyunTaek Oh <oht222@naver.com>
2024-02-12 22:41:05 +0900

    fix: 배포스크립트 템플릿 처리

--- packages/portfolio/scripts/deploy.js
+++ packages/portfolio/scripts/deploy.js
@@ -1,14 +1,15 @@
 const TOKEN = process.argv[2] || null
 
 const ghpages = require('gh-pages')
+const profile = require('../config/profile.json')
 
 ghpages.publish(
   'public',
   {
     branch: 'main',
     repo: TOKEN
-      ? `https://HyunTaek5:${TOKEN}@github.com/HyunTaek5/justsloth.git`
-      : 'https://github.com/HyunTaek5/justsloth.git'
+      ? `https://${profile.github}:${TOKEN}@github.com/유저이름/레포명.git`
+      : 'https://github.com/유저이름/레포명.git'
   },
   function (err) {
     console.log(err)
